cb6109dd2573799a67952704b248e66b9a7977fb
sparkar14 <ayulztron12@gmail.com>
2025-08-24 15:40:51 +0530

    .\.git\config

---
+++ src/test/java/com/tourism/NomadsLog/NomadsLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourism.NomadsLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadsLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
